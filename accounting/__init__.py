9adb1b333be51cdbf692e090940f50e1785f587d
rian bogle <rbogle@usgs.gov>
2017-01-10 15:23:55 -0700

    rename accounting

--- accounting/__init__.py
+++
@@ -1,2 +0,0 @@
-from resources import UserAssocApi, JobHistoryApi
-from database import db_session, init_db
--- accounting/config.py
+++
@@ -1,4 +0,0 @@
-DB_HOST = 'localhost'
-DB_USER = 'slurm'
-DB_PASS = 'slurm'
-DB_NAME = 'slurm_db_acct'
